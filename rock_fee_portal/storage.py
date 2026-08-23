6b76fdd9d672d5232a4a7d94338eeb2ca2afe9a8
BRIGHTDIC01 <thankgod.bright@binghamuni.edu.ng>
2026-08-23 12:28:50 +0100

    Fix duplicated payment proof storage path

--- rock_fee_portal/storage.py
+++ rock_fee_portal/storage.py
@@ -2,7 +2,6 @@ from storages.backends.s3 import S3Storage
 
 
 class SupabaseStorage(S3Storage):
-    location = "payment_proofs"
     file_overwrite = False
     default_acl = None
     signature_version = "s3v4"
